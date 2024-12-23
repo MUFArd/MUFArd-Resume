f418c0bd214dd9d7d6ecd05bd5f428f41fcdb458
MUFArd <muhammadfarelandriani@gmail.com>
2024-12-23 12:46:39 +0700

    update
    update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const Navlink = document.querySelectorAll ("nav > ul > li > a");
 
 Navlink.forEach(link => {
     link.addEventListener("click",function (event) {
+        event.preventDefault();
 
         Navlink.forEach(nav => nav.classList.remove("active"));
         
